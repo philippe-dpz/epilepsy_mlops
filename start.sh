203e33de51bfb6ec2cce703b52b81652ff64ea4a
philippe-dpz <philippedpz@gmail.com>
2025-07-29 09:47:19 +0200

    Mise à jour : service build

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-docker compose pull
+#docker compose pull
 
 echo "Démarrage de Prefect Server et MLflow..."
 docker compose up -d prefect_server mlflow
@@ -19,14 +19,15 @@ until curl -sSf http://localhost:5000 > /dev/null; do
 done
 
 echo "Démarrage des autres services..."
-docker compose up -d prefect_orchestrator auth_api inference_api patient_data_pull prometheus grafana
+docker compose up prefect_orchestrator 
+docker compoe up -d auth_api inference_api patient_data_pull prometheus grafana
 
 echo "Tous les services sont démarrés !"
 
 echo "URLs disponibles:"
 echo "- Prefect UI: http://localhost:4200"
 echo "- MLflow UI: http://localhost:5000" 
-echo "- Auth API: http://localhost:8000"
-echo "- Inference API: http://localhost:8001"
+echo "- Auth API: http://localhost:8000/docs"
+echo "- Inference API: http://localhost:8001/docs"
 echo "- Prometheus: http://localhost:9090"
 echo "- Grafana: http://localhost:3002"
